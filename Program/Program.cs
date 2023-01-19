82da046a6f1d2d54efb2bf8bc4a8488d354056ec
Shaun Moss <shaun@astromultimedia.com>
2023-01-19 11:15:18 +1000

    Updated template styles and partials.

---
+++ Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+DirectoryInfo dir = new (Environment.CurrentDirectory + "/../../../..");
+
+Console.WriteLine("Generating docs...");
+Console.WriteLine($"{dir.FullName}/docfx ./docfx.json");
+
+Process p = new ()
+{
+    StartInfo =
+    {
+        WorkingDirectory = dir.FullName,
+        FileName = "docfx",
+        Arguments = "./docfx.json",
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+    }
+};
+
+p.Start();
+p.WaitForExit();
